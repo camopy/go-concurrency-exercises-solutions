c2eb4f2477c02f9d5f49986d6c964f88202b90ed
Paulo Camopy <paulorenato.cpb@gmail.com>
2022-07-06 17:17:33 -0300

    feat: :sparkles: added limit service time solution
    using a ticer and channels for handling requests timeout

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,13 @@
 
 package main
 
+import (
+	"sync/atomic"
+	"time"
+)
+
+const MAX_TIME = 10
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,10 +28,51 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	if u.IsPremium {
+		return premiumProcess(process)
+	} else {
+		return freemiumProcess(process, u)
+	}
 }
 
 func main() {
 	RunMockServer()
+}
+
+func premiumProcess(process func()) bool {
+	process()
+	return true
+}
+
+func freemiumProcess(process func(), u *User) bool {
+	if u.IsOutOfTime() {
+		return false
+	}
+
+	done := make(chan bool)
+	tick := time.Tick(1 * time.Second)
+
+	go func() {
+		process()
+		done <- true
+	}()
+
+	for {
+		select {
+		case <-tick:
+			if t := u.AddTime(1); t >= MAX_TIME {
+				return false
+			}
+		case <-done:
+			return true
+		}
+	}
+}
+
+func (u *User) AddTime(seconds int64) int64 {
+	return atomic.AddInt64(&u.TimeUsed, seconds)
+}
+
+func (u *User) IsOutOfTime() bool {
+	return atomic.LoadInt64(&u.TimeUsed) >= MAX_TIME
 }
